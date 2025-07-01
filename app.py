c7c35a28bdf7e1f0be07f231d1d2e4a72182c154
HaDg <hatandung15@gmail.com>
2025-07-01 20:00:27 +0700

    now

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ LOGO_PATH = "logo.png"
 HLS_ROOT = "/tmp/hls"  # Thư mục tạm chứa HLS segments
 
 CHANNELS = {
-    "test": "https://7pal.short.gy/SkSpMainE"
+    "test": "https://7pal.short.gy/nowhkp1"
 }
 
 processes = {}
