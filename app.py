5ed5c17e24d8a0f9d4cb16995c7767fee8bf15ae
HaDg <hatandung15@gmail.com>
2025-07-01 19:49:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ LOGO_PATH = "logo.png"
 HLS_ROOT = "/tmp/hls"  # Thư mục tạm chứa HLS segments
 
 CHANNELS = {
-    "lamdong": "http://118.107.85.5:1935/live/smil:LTV.smil/playlist.m3u"
+    "lovenature": "https://d18dyiwu97wm6q.cloudfront.net/playlist720p.m3u8"
 }
 
 processes = {}
