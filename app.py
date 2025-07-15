5defa1947cba7b2c186227a111d3440a975c7d71
HaDg <hatandung15@gmail.com>
2025-07-15 21:10:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import shutil
 import atexit
 import logging
 import time
+import requests
 
 # --- Cấu hình Logging ---
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
